9d770869284cc5da87a43318237fb22fd757fb4a
alWestDeden <baboumediene@laposte.net>
2023-06-14 17:02:05 +0200

    add a delete project option (but page reload)

--- FrontEnd/assets/script/edition.js
+++ FrontEnd/assets/script/edition.js
@@ -1,88 +1,95 @@
-// build theBanner in Edition Mode
+// build the Banner in Edition Mode
 function buildEditionBanner() {
-    const editionBanner = document.createElement('div');
-    editionBanner.classList.add("edition-banner");
-    editionBanner.id = "edition-banner";
     const iconEdition = document.createElement('i');
     iconEdition.className = "fa-regular fa-pen-to-square";
     const textEdition = document.createElement('a');
     textEdition.innerText = "Mode édition";
     textEdition.id = "edition";
     const buttonEdition = document.createElement('button');
-    buttonEdition.classList.add("button", "button-edition");
+    buttonEdition.className = "button button-edition";
     buttonEdition.innerText = "publier les changements";
-    let bannerItems = [iconEdition, textEdition, buttonEdition];
-    for (let i in bannerItems) {
-        editionBanner.appendChild(bannerItems[i]);
-    }
+    const editionBanner = document.createElement('div');
+    editionBanner.className = "edition-banner";
+    editionBanner.id = "edition-banner";
+    editionBanner.append(iconEdition, textEdition, buttonEdition);
     document.querySelector('body').prepend(editionBanner);
 }
 
+// build the Pop-Up's elements
 function buildPopUp(works) {
-    // build the Pop-Up's elements
     const IconClosePopUp = document.createElement('i');
-    IconClosePopUp.id = "Pop-Up--Close"
+    IconClosePopUp.id = "crossIcon"
     IconClosePopUp.className = "fa-solid fa-xmark";
     const titlePopUp = document.createElement('h3');
     titlePopUp.innerText = "Galerie photo";
+    const alertPopUp = document.createElement('h4');
+    alertPopUp.id = "popup--alert";
+    alertPopUp.innerText = "";
     const galleryPopUp = document.createElement('div');
-    galleryPopUp.className = "galleryPopUp";
+    galleryPopUp.className = "popup--gallery";
     const linePopUp = document.createElement('div');
-    linePopUp.className = "pop-up--line";
+    linePopUp.className = "popup--line";
     const buttonPopUp = document.createElement('button');
     buttonPopUp.innerText = "Ajouter une photo";
     buttonPopUp.className = "button button--active";
     const deleteGalleryPopUp = document.createElement('a');
     deleteGalleryPopUp.innerText = "Supprimer la galerie";
-    // build the Pop-Up
-    const backgroundPopUp = document.createElement('div');
-    backgroundPopUp.className = "pop-up__background";
+    const overlayPopUp = document.createElement('div');
+    overlayPopUp.className = "overlay";
     const PopUp = document.createElement('aside');
-    PopUp.className = "pop-up";
-    PopUp.append(IconClosePopUp, titlePopUp, galleryPopUp, linePopUp, buttonPopUp, deleteGalleryPopUp)
-    backgroundPopUp.append(PopUp);
-    document.querySelector('body').prepend(backgroundPopUp);
-    // delete the content of the gallery before importing from Server
-    document.querySelector(".galleryPopUp").textContent = "";
-    buildGallery(works, "popup")
-    closePopUp(backgroundPopUp);
+    PopUp.className = "popup";
+    PopUp.append(IconClosePopUp, titlePopUp, alertPopUp, galleryPopUp, linePopUp, buttonPopUp, deleteGalleryPopUp)
+    overlayPopUp.append(PopUp);
+    document.querySelector('body').prepend(overlayPopUp);
+    // build the popup Gallery
+    buildGallery(works, "popupGallery");
+    // run the function which close the Pop Up
+    closePopUp();
 }
 
-function closePopUp(backgroundPopUp) {
-    // listen to icon to close the Pop-Up
-    document.getElementById("Pop-Up--Close").addEventListener("click", (event) => {
-        backgroundPopUp.remove();
+// function which close the Pop-Up
+function closePopUp() {
+    // listen to the overlay
+    const overlayPopUp = document.querySelector(".overlay");
+    // listen to the cross icon
+    document.getElementById("crossIcon").addEventListener('click', (event) => {
+        overlayPopUp.remove();
     })
-    // listen to icon to close the Pop-Up
-    backgroundPopUp.addEventListener("click", (event) => {
-        if (event.target.className === "pop-up__background") {backgroundPopUp.remove()};
+    overlayPopUp.addEventListener('click', (event) => {
+        if (event.target.className === "overlay") { overlayPopUp.remove() };
     })
 }
 
+// function to delete selected work
 function deleteWork(works) {
+    // listen to the trash icons
     for (let i in works) {
-        let trashIcon = document.getElementById(works[i].id);
+        let trashIcon = document.getElementById(`trash--${works[i].id}`);
         trashIcon.addEventListener('click', async(event) => {
             event.preventDefault();
-            let workID = event.target.getAttribute('id');
-            let token = sessionStorage.getItem("token");
-            fetch(`http://localhost:5678/api/works/${workID}`, {
-                method: 'DELETE',
-                mode: 'cors',
-                headers: {
-                    'Authorization': 'Bearer ${token}'
-                },
-            });
-            /*.then(function(response) {
+            event.stopPropagation;
+            let workID = event.target.getAttribute('id').replace("trash--", "");
+            let token = sessionStorage.getItem('token');
+            console.log(workID);
+            // delete clicked work from DataBase
+            const worksDB = await fetch(`http://localhost:5678/api/works/${workID}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    },
+            })
+            // if ok then rebuild the gallery
+            .then(function(response) {
                 if (response.ok) {
-                    buildGallery(works);
-                } else {
-                    console.log(error);
-                }
-            })*/
-            let work = document.getElementById(`figure--${workID}`);
-            console.log(work)
-            // work.remove();
-        })
+                    // works = worksDB.json();
+                    // print during 1s the deleted Project
+                    document.getElementById("popup--alert").innerText = `Suppression du projet: "${works[i].title}"`;
+                    // setTimeout(() => { document.getElementById("popup--alert").innerText = "" }, 1000);
+                    // erase the Pop-Up Gallery before rebuilding it
+                    // document.querySelector(".popup--gallery").innerText = '';
+                    // buildGallery(works, "popupGallery");
+                } else { document.getElementById("popup--alert").innerText = error; }
+            })
+        }) 
     }
 }
